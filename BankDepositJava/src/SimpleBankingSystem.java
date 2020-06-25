8137b7c0eb6a0ddd324262a5ca24a886caad5d6e
Victor Hugo <39852859+victorhgo@users.noreply.github.com>
2020-06-25 12:48:56 -0300

    Change all if/else statement with switch/case
    I'm learning Java, and I was searching for some Bank System ideas, and I find yours really good as a start point. But it works best with Switch/Case instead of if/else.

--- BankDepositJava/src/SimpleBankingSystem.java
+++ BankDepositJava/src/SimpleBankingSystem.java
@@ -7,38 +7,39 @@ public class SimpleBankingSystem {
 			Scanner input = new Scanner(System.in);
 
 			int n ;
-			do {
-				prn("Main Manu");
-				prn("1.Deposit");
-				prn("2.Withdraw");
-				prn("3.Balance");
-				prn("4.Exit");
-				prn("Your Choice :");
-				n = input.nextInt();
-				
+		do {
+            		prn("Main Manu");
+            		prn("1.Deposit");
+            		prn("2.Withdraw");
+            		prn("3.Balance");
+            		prn("4.Exit");
+            		prn("Your Choice :");
+			
+            		n = input.nextInt();
 
-				if(n == 1) {
-					prn("Enter the amount: ");
-					int a = input.nextInt();
-					deposit(a);
-					prn("\nYou successfully Deposit: " + a +"$\n");
-				}
-				
-				
-				
-				else if(n == 2) {
-					prn("Enter the amount: ");
-					int b = input.nextInt();
-					withdraw(b);
-					prn("\nYou successfully Withdraw: " + b +"$\n");
-				}
-				
-				
-				else if (n == 3) {
-					prn("\n #### Your current Balance is: "+balance +" ####\n");
-				}
-
-			}while(n != 4 );
+            		switch(n) {
+                		case 1:
+                    		    prn("Enter the amount: ");
+                    		    int a = input.nextInt();
+                                    deposit(a);
+                    		    prn("\nYou successfully Deposit: " + a +"$\n");
+                    	  	    break;
+                		case 2:
+                    		    prn("Enter the amount: ");
+                    		    int b = input.nextInt();
+                    		    withdraw(b);
+                    		    prn("\nYou successfully Withdraw: " + b +"$\n");
+                    		    break;
+                		case 3:
+                    		    prn("\n #### Your current Balance is: "+balance +" ####\n");
+				    break;	
+                 		case 4:
+                    		    break;
+                		default:
+                    		    prn("\n ERROR! Try Again\n");
+            		}
+        	}while(n != 4 );
+			
 
 
 		}
